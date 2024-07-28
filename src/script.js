8210a1aa6c87b801abcd4c7901886704545a9157
wrightrebecca <r3beccawright05@gmail.com>
2024-07-28 10:57:48 +0100

    Adding advanced search bar features - trial

--- src/script.js
+++ src/script.js
@@ -1,8 +1,19 @@
+function refreshWeather(response) {
+  console.log(response.data);
+}
+
+function searchCity(city) {
+  let apiKey = "37e9da10fet0ob8e3fe3769dc4fd8d70";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+  axios.get(apiUrl).then(refreshWeather);
+}
+
 function searchSubmitLocation(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-input");
   let cityElement = document.querySelector("#location");
   cityElement.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
