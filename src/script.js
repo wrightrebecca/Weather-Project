039ddbffbdfb60ee69d97e5d3cd3bc4d90f02262
wrightrebecca <r3beccawright05@gmail.com>
2024-07-29 12:08:22 +0100

    Integrate max and min temp API, and forecast icons

--- src/script.js
+++ src/script.js
@@ -81,22 +81,23 @@ function displayForecast(response) {
   console.log(response.data);
 
   let forecastElement = document.querySelector("#forecast");
-  let days = ["Mon", "Tues", "Weds", "Thurs", "Fri", "Sat"];
 
   let forecastHtml = "";
 
-  days.forEach(function (day) {
+  response.data.daily.forEach(function (day) {
     forecastHtml =
       forecastHtml +
       `
 <div class="weather-forecast-day">
-  <div class="weather-forecast-date">${day}</div>
-  <div class="weather-forecast-icon">🌤️</div>
+  <div class="weather-forecast-date">Tues</div>
+<img src ="${day.condition.icon_url}" class="weather-forecast-icon"/>
   <div class="weather-forecast-temperatures">
   <div class="weather-forecast-temperature">
-  <strong>15º</strong>
+  <strong>${Math.round(day.temperature.maximum)}º</strong>
   </div>
-  <div class="weather-forecast-temperature">9º</div>
+  <div class="weather-forecast-temperature">${Math.round(
+    day.temperature.minimum
+  )}º</div>
   </div>
   </div>`;
   });
