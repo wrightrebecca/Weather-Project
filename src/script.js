c1db80ca34faa0cf1ee886915277f8f146b06621
wrightrebecca <r3beccawright05@gmail.com>
2024-07-29 12:19:08 +0100

    Integrate days into forecast

--- src/script.js
+++ src/script.js
@@ -69,6 +69,13 @@ let currentDate = new Date();
 
 currentDateELement.innerHTML = formatDate(currentDate);
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thur", "Fri", "Sat"];
+
+  return days[date.getDay()];
+}
+
 function getForecast(city) {
   let apiKey = "37e9da10fet0ob8e3fe3769dc4fd8d70";
   let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
@@ -90,7 +97,7 @@ function displayForecast(response) {
         forecastHtml +
         `
 <div class="weather-forecast-day">
-  <div class="weather-forecast-date">Tues</div>
+  <div class="weather-forecast-date">${formatDay(day.time)}</div>
 <img src ="${day.condition.icon_url}" class="weather-forecast-icon"/>
   <div class="weather-forecast-temperatures">
   <div class="weather-forecast-temperature">
